cf05014d88c3ef60d1d489c3921161087b5bf801
UglyFatDead <julianviceroy124@gmail.com>
2021-12-28 17:50:53 -0300

    add compression and helmet

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
+
+app.use(compression());
+app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
